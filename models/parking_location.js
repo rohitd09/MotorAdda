670ff43c6dfc566397b5acf6faaa11519b9721dc
DESKTOP-S7FGUGR\Rohit Das <rohitd3476@gmail.ccom>
2021-10-14 23:33:50 +0530

    Completed My Parking Page

--- models/parking_location.js
+++ models/parking_location.js
@@ -4,6 +4,7 @@ const ParkingLocationSchema = new mongoose.Schema({
   parking_spot_owner: String,
   parking_spot_name: String,
   parking_spot_location: String,
+  parking_spot_address: String,
   number_of_available_spots: {
       type: Number,
       default: 0
@@ -11,11 +12,13 @@ const ParkingLocationSchema = new mongoose.Schema({
   open_time: {
     hour: {
       type: Number,
+      default: 0,
       min: 0,
       max: 23
     },
     minute: {
       type: Number,
+      default: 0,
       min: 0,
       max: 59
     }
@@ -23,11 +26,13 @@ const ParkingLocationSchema = new mongoose.Schema({
   close_time: {
     hour: {
       type: Number,
+      default: 0,
       min: 0,
       max: 23
     },
     minute: {
       type: Number,
+      default: 0,
       min: 0,
       max: 59
     }
